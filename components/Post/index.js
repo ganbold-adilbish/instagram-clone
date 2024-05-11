f36e1784f047aa25418d5b7790f74b8f11bdc8e3
ganbold-adilbish <ganbold.adilbish.111@gmail.com>
2024-05-11 20:58:27 +0800

    Add Post component

---
+++ components/Post/index.js
@@ -0,0 +1,64 @@
+import Image from "next/image";
+
+const actionButtons = [
+  {
+    id: 1,
+    name: "like",
+  },
+  {
+    id: 2,
+    name: "comment",
+  },
+  {
+    id: 3,
+    name: "share-post",
+  },
+];
+
+const saveButton = {
+  name: "save",
+};
+
+export default function Post({ url, description, likeNumber, name, isLiked }) {
+  return (
+    <div className="p-4 md:p-0 my-20 w-[468px] text-sm">
+      <Image
+        priority
+        src={url}
+        height={0}
+        width={0}
+        alt={"no picture"}
+        className="rounded-sm w-full"
+        sizes="100vw"
+      />
+      <div className="flex py-4 justify-between">
+        <div className="flex space-x-2">
+          {actionButtons.map(({ id, name }) => (
+            <button key={id}>
+              <Image
+                src={`/post/${name}${
+                  isLiked && name === "like" ? "-solid" : ""
+                }.svg`}
+                height={24}
+                width={24}
+                alt={name}
+              />
+            </button>
+          ))}
+        </div>
+        <button>
+          <Image
+            src={`/post/${saveButton.name}.svg`}
+            height={24}
+            width={24}
+            alt={saveButton.name}
+          />
+        </button>
+      </div>
+      <h1 className="font-bold">{likeNumber} likes</h1>
+      <p className="mt-2">
+        <span className="font-bold">{name}</span> {description}
+      </p>
+    </div>
+  );
+}
