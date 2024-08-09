ce8c8d6128c6a975fa9951bc1db082149211b163
ganbold-adilbish <ganbold.adilbish.111@gmail.com>
2024-08-09 17:12:39 +0800

    Add SpeedInsights

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
   title: "Instagram Clone",
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
