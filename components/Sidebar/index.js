105405d14d2ec8d3f33c3ce7cd74e1cbf5250862
Ganbold Adilbish <ganbold.adilbish.111@gmail.com>
2024-11-26 12:16:46 +0800

    Update Sidebar

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -4,18 +4,28 @@ import Footer from "../Footer";
 
 const suggestions = [];
 
-export default function Sidebar({ url, username, fullName }) {
+export default function Sidebar({
+  profile_image,
+  username,
+  first_name,
+  last_name,
+}) {
   return (
     <aside className="hidden lg:block">
       <div className="mt-4 flex flex-col space-y-4 w-full max-w-[320px]">
         <div className="flex justify-between">
           <div className="flex space-x-4">
             <div className="h-14 w-14 rounded-full flex-none overflow-hidden">
-              <Image src={url} height={56} width={56} alt="no pic" />
+              <Image
+                src={profile_image.large}
+                height={56}
+                width={56}
+                alt="no pic"
+              />
             </div>
             <div className="flex flex-col justify-center text-sm">
               <strong className="w-14 text-gray-500">{username}</strong>
-              <span>{fullName}</span>
+              <span>{`${first_name} ${last_name}`}</span>
             </div>
           </div>
           <button className="text-blue-500">Switch</button>
