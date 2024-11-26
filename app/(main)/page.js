912e0c95adbf3f761a9da8fc702f490176f6d03d
Ganbold Adilbish <ganbold.adilbish.111@gmail.com>
2024-11-26 12:25:38 +0800

    Add unsplash api to home page

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -1,86 +1,29 @@
 import Posts from "@/components/Posts";
 import Sidebar from "@/components/Sidebar";
 import Stories from "@/components/Stories";
-import { prisma } from "@/lib/prisma";
 
 async function getUser() {
-  let user = await prisma.user.findUnique({
-    where: {
-      username: "alexmorg123",
-    },
-    select: {
-      url: true,
-      username: true,
-      fullName: true,
-      feed: {
-        select: {
-          stories: {
-            where: {
-              published: true,
-            },
-            orderBy: {
-              createdAt: "desc",
-            },
-            select: {
-              id: true,
-              url: true,
-              name: true,
-            },
-          },
-          posts: {
-            where: {
-              published: true,
-            },
-            orderBy: {
-              createdAt: "desc",
-            },
-            select: {
-              _count: {
-                select: {
-                  usersWhoHaveLiked: true,
-                },
-              },
-              id: true,
-              url: true,
-              description: true,
-              author: {
-                select: {
-                  username: true,
-                },
-              },
-              usersWhoHaveLiked: {
-                where: {
-                  username: "alexmorg123",
-                },
-                select: {
-                  id: true,
-                },
-              },
-            },
-          },
-        },
+  const data = await fetch(
+    `${process.env.UNSPLASH_API_URL}/users/${process.env.USERNAME}`,
+    {
+      headers: {
+        Authorization: `Client-ID ${process.env.ACCESS_KEY}`,
       },
-    },
-  });
-
-  user.feed.posts = user.feed.posts.map((post) => ({
-    ...post,
-    likes_total_count: post._count.usersWhoHaveLiked,
-    has_liked: post.usersWhoHaveLiked.length === 1,
-  }));
-
-  return user;
+    }
+  );
+  const post = await data.json();
+  return post;
 }
 
 export default async function Home() {
   const user = await getUser();
-  const { posts, stories } = user.feed;
+  // const { posts, stories } = user.feed;
 
   return (
     <div className="flex space-x-10 mx-auto w-full max-w-[950px]">
       <div className="flex flex-col items-center w-full max-w-[655px]">
-        <Stories stories={stories} />
-        <Posts posts={posts} />
+        {/* <Stories stories={stories} />
+        <Posts posts={posts} /> */}
       </div>
       <Sidebar {...user} />
     </div>
