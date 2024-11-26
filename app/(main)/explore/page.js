65ab1a78ac94360947fe5b5d369d6a59b55fd63c
Ganbold Adilbish <ganbold.adilbish.111@gmail.com>
2024-11-26 11:30:23 +0800

    Add unsplash to explore page

--- app/(main)/explore/page.js
+++ app/(main)/explore/page.js
@@ -1,29 +1,29 @@
-import { prisma } from "@/lib/prisma";
 import Image from "next/image";
 import Link from "next/link";
 
 async function getPosts() {
-  const posts = await prisma.post.findMany({
-    where: {
-      published: true,
+  const data = await fetch(`${process.env.UNSPLASH_API_URL}`, {
+    headers: {
+      Authorization: `Client-ID ${process.env.ACCESS_KEY}`,
     },
   });
-  posts.sort(() => 0.5 - Math.random());
-
+  const posts = await data.json();
   return posts;
 }
+
+export const dynamic = "force-dynamic";
 
 export default async function Explore() {
   const posts = await getPosts();
 
   return (
     <div className="px-3 my-10 grid grid-cols-3 gap-1  mx-auto w-full max-w-[992px]">
-      {posts.map(({ id, url }) => (
+      {posts.map(({ id, urls }) => (
         <Link key={id} href={`/p/${id}`}>
           <div className="relative overflow-hidden pb-[100%]">
             <Image
               priority
-              src={url}
+              src={urls.regular}
               fill
               alt="no picture"
               className="object-cover"
