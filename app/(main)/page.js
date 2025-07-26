df48edefd5f83d91fc54b4859074114e376c11f6
Ganbold Adilbish <ganbold.adilbish.111@gmail.com>
2025-07-26 19:29:06 +0800

    Update Home page with data fetching for posts and stories

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -11,19 +11,46 @@ async function getUser() {
       },
     }
   );
-  const post = await data.json();
-  return post;
+  const user = await data.json();
+  return user;
+}
+
+async function getPosts() {
+  const data = await fetch(
+    `${process.env.UNSPLASH_API_URL}/photos?page=1&per_page=30`,
+    {
+      headers: {
+        Authorization: `Client-ID ${process.env.ACCESS_KEY}`,
+      },
+    }
+  );
+  const posts = await data.json();
+  return posts;
+}
+
+async function getStories() {
+  const data = await fetch(
+    `${process.env.UNSPLASH_API_URL}/search/users?query=tom&per_page=25`,
+    {
+      headers: {
+        Authorization: `Client-ID ${process.env.ACCESS_KEY}`,
+      },
+    }
+  );
+  const { results } = await data.json();
+  return results;
 }
 
 export default async function Home() {
   const user = await getUser();
-  // const { posts, stories } = user.feed;
+  const posts = await getPosts();
+  const stories = await getStories();
 
   return (
     <div className="flex space-x-10 mx-auto w-full max-w-[950px]">
       <div className="flex flex-col items-center w-full max-w-[655px]">
-        {/* <Stories stories={stories} />
-        <Posts posts={posts} /> */}
+        <Stories stories={stories} />
+        <Posts posts={posts} />
       </div>
       <Sidebar {...user} />
     </div>
