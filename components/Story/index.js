e2fdfeb7be3ce82570971ec81c0dd7d8fbe44afb
Ganbold Adilbish <ganbold.adilbish.111@gmail.com>
2025-07-26 19:21:55 +0800

    Update Story component with new design and better image handling

--- components/Story/index.js
+++ components/Story/index.js
@@ -1,12 +1,21 @@
 import Image from "next/image";
 
-export default function Story({ url, name }) {
+export default function Story({ profile_image, name }) {
   return (
-    <button className="flex flex-col items-center space-y-2">
-      <div className="h-14 w-14 rounded-full overflow-hidden flex-none ring-2 ring-gray-200 ring-offset-2">
-        <Image src={url} height={56} width={56} alt="no pic" />
+    <button className="flex flex-col items-center">
+      <div className="p-2 pt-1">
+        <div className="h-[74px] w-[74px] rounded-full overflow-hidden flex-none ring-2 ring-[rgb(204_204_204)] ring-offset-2">
+          <Image
+            src={profile_image.large}
+            height={74}
+            width={74}
+            alt="no pic"
+          />
+        </div>
       </div>
-      <h6 className="truncate w-14 text-xs text-gray-500">{name}</h6>
+      <h6 className="truncate max-w-[66px] text-sm/[18px] h-4 lowercase">
+        {name}
+      </h6>
     </button>
   );
 }
