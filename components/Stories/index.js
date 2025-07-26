74d1a209b538605ce07ed07f688ffcfe6ee5a24f
Ganbold Adilbish <ganbold.adilbish.111@gmail.com>
2025-07-26 19:24:49 +0800

    Update Stories component with improved layout

--- components/Stories/index.js
+++ components/Stories/index.js
@@ -2,7 +2,7 @@ import Story from "../Story";
 
 export default function Stories({ stories }) {
   return (
-    <div className="flex space-x-4 p-4 w-full max-w-[655px] overflow-x-auto">
+    <div className="flex space-x-4 px-[9px] py-2 w-full max-w-[630px] overflow-x-auto">
       {stories.map((story) => (
         <Story key={story.id} {...story} />
       ))}
